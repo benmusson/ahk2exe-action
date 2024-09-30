6b162946e013a2b54cb8846a6ddf4743b295607f
benmusson <ben.d.musson@gmail.com>
2024-09-29 21:58:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# action-ahk2exe-v2
+GitHub Action for building [AutoHotkey v2] scripts with [Ahk2Exe].
+
+> [!NOTE]  
+> This action only support AutoHotkey version 2.
+
+![Tests](https://img.shields.io/github/actions/workflow/status/benmusson/action-ahk2exe-v2/test.yml?label=tests)
+
+## Usage example
+
+> [!IMPORTANT]  
+> This action only works on `windows` GitHub actions runners.
+
+
+[action-ahk2exe-v2]: https://github.com/benmusson/action-ahk2exe-v2
+[AutoHotkey]: https://github.com/AutoHotkey/AutoHotkey
+[AutoHotkey v2]: https://github.com/AutoHotkey/AutoHotkey
+[Ahk2Exe]: https://github.com/AutoHotkey/Ahk2Exe
---
+++ ahk2exe-v2-action.ps1
@@ -0,0 +1,124 @@
+param (
+    [string]$in,
+    [string]$out,
+    [string]$icon,
+    [string]$target = 'x64',
+    [string]$compression = 'upx',
+    [string]$url_ahk = 'https://github.com/AutoHotkey/AutoHotkey/releases/download/v2.0.18/AutoHotkey_2.0.18.zip',
+    [string]$url_ahk2exe = 'https://github.com/AutoHotkey/Ahk2Exe/releases/download/Ahk2Exe1.1.37.02a0/Ahk2Exe1.1.37.02a0.zip',
+    [string]$url_upx = 'https://github.com/upx/upx/releases/download/v4.2.4/upx-4.2.4-win64.zip',
+    [string]$build_assets_folder = '.\.ahk2exe-v2-action'
+)
+
+$ErrorActionPreference = "Stop"
+
+$path_assets = Join-Path $build_assets_folder 'assets'
+$path_downloads = Join-Path $build_assets_folder 'downloads'
+
+function Show-Message {
+    param (
+        [string]$header,
+        [string]$message,
+        [string]$header_color = "Green",
+        [string]$message_color = "White"
+    )
+    Write-Host "::$header:: " -ForegroundColor $header_color -NoNewLine
+    Write-Host "$message" -ForegroundColor $message_color
+}
+
+function Invoke-DownloadArtifacts {
+    param (
+        [string]$name, 
+        [string]$url
+    )
+    $path_zip = Join-Path $path_downloads "$name.zip"
+    $path_final = Join-Path $path_assets $name
+
+    if (Test-Path -Path $path_final) { 
+        if ((Get-ChildItem -Path "$path_final" | Measure-Object).Count -gt 0) {
+            Show-Message "Download $name" "$name is already present, skipping re-download..." "Blue" "DarkGray"
+            return
+        }
+    }
+    Show-Message "Download $name" "Downloading..." "Blue" "DarkGreen"
+    Show-Message "Download $name" "Source: $url" "Blue" "DarkYellow"
+    Show-Message "Download $name" "Destination: $path_zip" "Blue" "DarkYellow"
+    
+    [void](New-Item -ItemType Directory -Path $path_downloads -Force)
+    [void](New-Object System.Net.WebClient).DownloadFile($url, $path_zip)
+
+    [void](New-Item -ItemType Directory -Path $path_assets -Force)
+    Expand-Archive -Force $path_zip -DestinationPath $path_final
+
+    Show-Message "Download $name" "Download completed" "Blue" "Magenta"
+}
+
+function Install-UPX {
+    $destination = Join-Path "$path_assets" "Ahk2Exe\upx.exe"
+    if ([System.IO.File]::Exists($destination)) {
+        Show-Message "Install UPX" "UPX is already installed, skipping installation..." "Blue" "DarkGray"
+        return
+    }
+
+    Show-Message "Install UPX" "Searching for UPX executable..." "Blue" "DarkGreen"
+    foreach ($exe in Get-ChildItem -Path (Join-Path $path_assets "UPX") -Filter *.exe -Recurse)  {
+        Show-Message "Install UPX" "Found, copying to $destination" "Blue" "Yellow"
+        Move-Item -Path $exe.FullName -Destination $destination -Force
+        break
+    }
+
+    if ([System.IO.File]::Exists($destination)) {
+        Show-Message "Install UPX" "Installation Successful" "Blue" "Magenta"
+    } else {
+        throw "Failed to install UPX. File was not present in Ahk2Exe folder after installation step completed."
+    }
+}
+
+function Invoke-Ahk2Exe {
+    param (
+        [string]$in,
+        [string]$out,
+        [string]$icon,
+        [string]$target = 'x64',
+        [string]$compression = 'upx'
+    )
+    Show-Message "Build $out" "Converting $ahk_input to $out..." "Blue" "DarkGreen"
+
+    Switch ($target) {
+        'x64' { $ahk = Join-Path $path_assets 'AutoHotkey/AutoHotkey64.exe' }
+        'x86' { $ahk = Join-Path $path_assets 'AutoHotkey/AutoHotkey32.exe' }
+        Default { Throw "Unsupported Architecture: '$arch'. Valid Options: x64, x86" }
+    }
+
+    $ahk2exe_path = Join-Path $path_assets 'Ahk2Exe/Ahk2Exe.exe'
+    $ahk2exe_args = "/silent verbose /base `"$ahk`" /in `"$in`""
+
+    $ahk2exe_args += if (![string]::IsNullOrEmpty($out)) { " /out `"$out`"" }
+    $ahk2exe_args += if (![string]::IsNullOrEmpty($icon)) { " /icon `"$icon`"" }
+
+    Switch ($compression) {
+        'none' { $ahk2exe_args += " /compress 0" }
+        'upx'  { $ahk2exe_args += " /compress 2" } 
+        Default { Throw "Unsupported Compression Type: '$compression'. Valid Options: none, upx"}
+    }
+
+    $command = "Start-Process -NoNewWindow -Wait -FilePath `"$ahk2exe_path`" -ArgumentList '$ahk2exe_args'"
+
+    Show-Message "Build $out" "`"$command`"" "Blue" "DarkYellow"
+    Invoke-Expression "$command"
+    Show-Message "Build $out" "Build completed" "Blue" "Magenta"
+}
+
+Show-Message "Build Started" "" "Magenta"
+
+Invoke-DownloadArtifacts 'AutoHotkey' "$url_ahk"
+Invoke-DownloadArtifacts 'Ahk2Exe' "$url_ahk2exe"
+
+if ("$compression" -eq "upx") {
+    Invoke-ArtifactsDownload 'UPX' "$url_upx"
+    Install-UPX
+}
+
+Invoke-Ahk2Exe -In "$in" -Out "$out" -Icon "$icon" -Target "$target" -Compression "$compression"
+
+Show-Message "Build Finished" "" "Magenta"
