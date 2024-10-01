d6f281c566affe75f3e9cdda63ada1ce6a95a497
Ben Musson <58087114+benmusson@users.noreply.github.com>
2024-09-30 21:24:56 -0400

    Merge Snapshot Release (#1)

--- README.md
+++ README.md
@@ -1,18 +1,164 @@
-# action-ahk2exe-v2
-GitHub Action for building [AutoHotkey v2] scripts with [Ahk2Exe].
+# Ahk2Exe Action
+This action allows compiling [AutoHotkey] scripts with [Ahk2Exe].
 
-> [!NOTE]  
-> This action only support AutoHotkey version 2.
+![Build and Test](https://img.shields.io/github/actions/workflow/status/benmusson/ahk2exe-action/test.yml?label=tests)
 
-![Tests](https://img.shields.io/github/actions/workflow/status/benmusson/action-ahk2exe-v2/test.yml?label=tests)
+| **Feature** | **Description** |
+| - | - |
+| **Support for AutoHotkey v1.1 and v2.0**          | 🎉 Works with both AutoHotkey v1.1 and v2.0, so you can use whichever version you prefer!                         |
+| **UPX Compression Support**                       | 📦 Option to use UPX compression for smaller, more lightweight binaries.                                          |
+| **Cache Support**                                 | ⚡ Built-in caching to speed things up by avoiding unnecessary downloads on repeated tasks.                       |
+| **Direct Downloads from AutoHotkey GitHub Repos** | ⬇️ Grab binaries directly from the official AutoHotkey repos, with the option to pick the release tag you need.   |
+| **No External GitHub Actions Needed**             | 🚀 Fully self-contained! No need for external GitHub Actions – everything works out of the box.                   |
 
-## Usage example
+
+## Usage
+
+### Pre-requisites
 
 > [!IMPORTANT]  
 > This action only works on `windows` GitHub actions runners.
 
+### Complete Example
+```yaml
+name: Compile
 
-[action-ahk2exe-v2]: https://github.com/benmusson/action-ahk2exe-v2
+on: push
+
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v4
+
+    - name: Build with latest AutoHotkey release
+      uses: benmusson/ahk2exe-action@v1
+      with:
+        in: .\path\to\script.ahk
+        out: .\build\MyCompiledScript.exe
+        icon: .\images\favicon.ico
+        target: x64
+        compression: upx
+        ahk-tag: latest
+```
+
+### Inputs
+
+| Name | Description | Default | Required |
+|-|-|-|-|
+|`in`| The path and name of the script to compile. | | ✔️
+|`out`| The path\name of the output .exe to be created. | The directory\base_name of the input file plus extension of .exe, or any relevant compiler directive in the script. |
+|`icon`| The icon file to be used. | Any SetMainIcon compiler directive in the script. |
+|`target`| Target architecture. Valid options: x86, x64. | `x64` |
+|`resourceid`| Assigns a non-standard resource ID to be used for the main script for compilations which use an .exe base file (see Embedded Scripts). Numeric resource IDs should consist of a hash sign (#) followed by a decimal number. | #1, or any ResourceID compiler directive in the script. |
+|`compression`| Specifies which compression method to use. Valid options: none, upx. | `upx` |
+|`ahk-repo`| GitHub repository source for AutoHotkey. | `AutoHotkey/AutoHotkey` |
+|`ahk-tag`| Tagged GitHub release for AutoHotkey. View releases for official repo [here](https://github.com/AutoHotkey/AutoHotkey/releases). | `latest` |
+|`ahk2exe-repo`| GitHub repository source for Ahk2Exe. | `AutoHotkey/Ahk2Exe` |
+|`ahk2exe-tag`| Tagged GitHub release for Ahk2Exe. View releases for official repo [here](https://github.com/AutoHotkey/Ahk2Exe/releases). | `latest` |
+|`upx-repo`| GitHub repository source for UPX. | `UPX/UPX` |
+|`upx-tag`| Tagged GitHub release for UPX. View releases for official repo [here](https://github.com/UPX/UPX/releases).| `latest` |
+|`build-assets-folder`| The path used to store build assets downloaded during the action. | `.\.ahk2exe-v2-action` |
+
+### Example Workflows
+
+#### Basic Usage
+```yaml
+name: Compile
+
+on: push
+
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v4
+
+      # Compiled file will be '.\path\to\script.exe'
+    - name: Build with latest AutoHotkey release
+      uses: benmusson/ahk2exe-action@v1
+      with:
+        in: .\path\to\script.ahk
+```
+
+#### Specify AutoHotkey Version
+```yaml
+name: Compile
+
+on: push
+
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v4
+
+    - name: Build with AutoHotkey v1.1.37.02
+      uses: benmusson/ahk2exe-action@v1
+      with:
+        in: .\path\to\script.ahk
+        ahk-tag: v1.1.37.02
+```
+
+
+#### Specify Output, Use Icon
+```yaml
+name: Compile
+
+on: push
+
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v4
+
+    - name: Build (With Icon)
+      uses: benmusson/ahk2exe-action@v1
+      with:
+        in: .\path\to\script.ahk
+        out: .\build\MyCompiledScript.exe
+        icon: .\images\favicon.ico
+```
+
+#### Build for x86 and x64
+```yaml
+name: Compile (x86/x64)
+
+on: push
+
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v4
+
+    - name: Build (x86)
+      uses: benmusson/ahk2exe-action@v1
+      with:
+        in: .\path\to\script.ahk
+        out: .\build\MyCompiledScript_x86.exe
+        icon: .\images\favicon.ico
+        target: x86
+        ahk-tag: v2.0.18
+
+    - name: Build (x64)
+      uses: benmusson/ahk2exe-action@v1
+      with:
+        in: .\path\to\script.ahk
+        out: .\build\MyCompiledScript_x64.exe
+        icon: .\images\favicon.ico
+        target: x64
+        ahk-tag: v2.0.18
+```
+
+## Contributing
+Have an issue or see room for improvement? Issues and pull requests are welcome!
+
+## License
+The scripts and documentation in this project are released under the [GPLv3 License](https://github.com/benmusson/ahk2exe-action/blob/main/LICENSE).
+
+[action-ahk2exe]: https://github.com/benmusson/action-ahk2exe
 [AutoHotkey]: https://github.com/AutoHotkey/AutoHotkey
-[AutoHotkey v2]: https://github.com/AutoHotkey/AutoHotkey
 [Ahk2Exe]: https://github.com/AutoHotkey/Ahk2Exe
+[GPLv3 License]: (https://github.com/benmusson/ahk2exe-action/blob/main/LICENSE)
---
+++ action.ps1
@@ -0,0 +1,236 @@
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+$PathAssets = $env:BuildAssetsFolder
+$PathDownloads = $env:BuildAssetsFolder
+
+$StyleInfo = $PSStyle.Foreground.Blue
+$StyleAction = $PSStyle.Foreground.Green
+$StyleStatus = $PSStyle.Foreground.Magenta
+$StyleCommand = $PSStyle.Foreground.Yellow
+$StyleQuiet = $PSStyle.Foreground.BrightBlack
+
+function Show-Message {
+    param (
+        [string]$header,
+        [string]$message,
+        [string]$header_style = $PSStyle.Foreground.Blue,
+        [string]$message_style = $PSStyle.Foreground.White
+    )
+    Write-Host "$header_style::$header::$($PSStyle.Reset) " -NoNewLine
+    Write-Host "$message_style$message$($PSStyle.Reset)"
+}
+
+function Get-GitHubReleaseAssets {
+    param (
+        [string]$Repository,
+        [string]$ReleaseTag = 'latest',
+        [string]$FileTypeFilter
+    )
+
+    $repositoryOwner, $repositoryName = ($Repository -split "/")[0, 1]
+    if ([string]::IsNullOrEmpty($repositoryOwner)) { Throw "Invalid repository path, missing repository owner."}
+    if ([string]::IsNullOrEmpty($repositoryName)) { Throw "Invalid repository path, missing repository name."}
+
+    $displayPath = "$repositoryOwner/$repositoryName/$ReleaseTag"
+    $downloadFolderName = $displayPath.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+    $downloadFolder = Join-Path $PathDownloads $downloadFolderName
+    if (Test-Path -Path $downloadFolder) { 
+        if ((Get-ChildItem -Path "$downloadFolder" | Measure-Object).Count -gt 0) {
+            Show-Message "Download-$displayPath" "$displayPath is already present, skipping re-download..." $StyleInfo $StyleQuiet
+            return $downloadFolder
+        }
+    }
+
+    if ($ReleaseTag -like 'latest') {
+        $apiUrl = "https://api.github.com/repos/$repositoryOwner/$repositoryName/releases/latest"
+    } else {
+        $apiUrl = "https://api.github.com/repos/$repositoryOwner/$repositoryName/releases/tags/$ReleaseTag"
+    }
+
+    Show-Message "Download-$displayPath" "Getting release information..." $StyleInfo $StyleAction
+    $release = Invoke-RestMethod -Uri $apiUrl -Method Get -Headers @{ "User-Agent" = "PowerShell" }
+
+    $assets = $release.assets
+    if ($assets.Count -eq 0) { Throw "No assets found for release '$displayPath'" }
+
+    Show-Message "Download-$displayPath" "Filtering assets for '$FileTypeFilter' files..." $StyleInfo $StyleAction
+    $filteredAssets = $assets | Where-Object { $_.name -like "$FileTypeFilter" }
+    if ($filteredAssets.Count -eq 0) { Throw "No assets matching the file type '$FileTypeFilter' found." }
+    Show-Message "Download-$displayPath" "Found files: $filteredAssets" $StyleInfo $StyleCommand
+
+    Show-Message "Download-$displayPath" "Downloading assets..." $StyleInfo $StyleAction
+    foreach ($asset in $filteredAssets) {
+        $downloadUrl = $asset.browser_download_url
+        $fileName = $asset.name
+        $downloadDestination = Join-Path $downloadFolder $fileName
+
+        Show-Message "Asset-$fileName" "Downloading..." $StyleInfo $StyleAction
+        Show-Message "Asset-$fileName" "Source: $downloadUrl" $StyleInfo $StyleCommand
+        Show-Message "Asset-$fileName" "Destination: $downloadDestination" $StyleInfo $StyleCommand
+        [void](New-Item -ItemType Directory -Path $downloadFolder -Force)
+        [void](New-Object System.Net.WebClient).DownloadFile($downloadUrl, $downloadDestination)
+        Show-Message "Asset-$fileName" "Download completed" $StyleInfo $StyleStatus
+    }
+    Show-Message "Download-$displayPath" "Downloading assets completed" $StyleInfo $StyleStatus
+    return $downloadFolder
+}
+
+function Invoke-UnzipAllInPlace {
+    param (
+        [string]$TaskName,
+        [string]$FolderPath
+    )
+
+    foreach ($zip in Get-ChildItem -Path $FolderPath -Filter *.zip -Recurse) {
+        Show-Message "$TaskName" "Extracting $zip..." $StyleInfo $StyleAction
+        Show-Message "$TaskName" "Source: $zip" $StyleInfo $StyleCommand
+        Show-Message "$TaskName" "Destination: $FolderPath" $StyleInfo $StyleCommand
+        [void](New-Item -ItemType Directory -Path $PathAssets -Force)
+        Expand-Archive -Force $zip -DestinationPath $FolderPath
+    }
+}
+
+function Install-AutoHotkey {
+    Show-Message "Install-Autohotkey" "Installing..." $StyleInfo $StyleAction
+    $downloadFolder = Get-GitHubReleaseAssets -Repository "$env:AutoHotkeyRepo" -ReleaseTag "$env:AutoHotkeyTag" -FileTypeFilter "*.zip"
+
+    switch ($env:Target) {
+        'x64' { 
+            $exeName = 'AutoHotkey64.exe' 
+            $searchFilter = 'AutoHotkey(U)?64\.exe'
+        }
+        'x86' { 
+            $exeName = 'AutoHotkey32.exe' 
+            $searchFilter = 'AutoHotkey(U)?32\.exe'
+        }
+        Default { Throw "Unsupported Architecture: '$target'. Valid Options: x64, x86" }
+    }
+
+    $installPath = (Get-ChildItem -Path $downloadFolder -Recurse | Where-Object { $_.Name -match "^$searchFilter$" } | Select-Object -First 1)
+    if ([System.IO.File]::Exists($installPath)) { 
+        Show-Message "Install-Autohotkey" "Autohotkey is already installed, skipping re-installation..." $StyleInfo $StyleQuiet
+        return $installPath
+    }
+
+    Invoke-UnzipAllInPlace -TaskName "Install-Autohotkey" -FolderPath $downloadFolder
+
+    Show-Message "Install-Autohotkey" "Verifying installation..." $StyleInfo $StyleAction
+    $installPath = (Get-ChildItem -Path $downloadFolder -Recurse | Where-Object { $_.Name -match "^$searchFilter$" } | Select-Object -First 1)
+    if (![System.IO.File]::Exists($installPath)) { Throw "Missing AutoHotkey Executable '$exeName'." }
+    Show-Message "Install-Autohotkey" "Installation path: $installPath" $StyleInfo $StyleCommand
+    Show-Message "Install-Autohotkey" "Installation completed" $StyleInfo $StyleStatus
+    return $installPath
+}
+
+function Install-Ahk2Exe {
+    Show-Message "Install-Ahk2Exe" "Installing..." $StyleInfo $StyleAction
+    $downloadFolder = Get-GitHubReleaseAssets -Repository "$env:Ahk2ExeRepo" -ReleaseTag "$env:Ahk2ExeTag" -FileTypeFilter "*.zip"
+
+    $exeName = 'Ahk2Exe.exe'
+
+    $installPath = (Get-ChildItem -Path $downloadFolder -Recurse -Filter $exeName | Select-Object -First 1)
+    if ([System.IO.File]::Exists($installPath)) { 
+        Show-Message "Install-Ahk2Exe" "Ahk2Exe is already installed, skipping re-installation..." $StyleInfo $StyleQuiet
+        return $installPath
+    }
+
+    Invoke-UnzipAllInPlace -TaskName "Install-Ahk2Exe" -FolderPath $downloadFolder
+
+    Show-Message "Install-Ahk2Exe" "Verifying installation..." $StyleInfo $StyleAction
+    $installPath = (Get-ChildItem -Path $downloadFolder -Recurse -Filter $exeName | Select-Object -First 1)
+    if (![System.IO.File]::Exists($installPath)) { Throw "Missing Ahk2Exe Executable '$exeName'." }
+    Show-Message "Install-Ahk2Exe" "Installation path: $installPath" $StyleInfo $StyleCommand
+    Show-Message "Install-Ahk2Exe" "Installation completed" $StyleInfo $StyleStatus
+    return $installPath
+}
+
+function Install-UPX {
+    param (
+        [string]$Ahk2ExePath
+    )
+
+    Show-Message "Install-UPX" "Installing..." $StyleInfo $StyleAction
+    $downloadFolder = Get-GitHubReleaseAssets -Repository "$env:UPXRepo" -ReleaseTag "$env:UPXTag" -FileTypeFilter "*win64.zip"
+
+    $exeName = 'upx.exe'
+    $ahk2exeFolder = Split-Path -Path $Ahk2ExePath -Parent 
+
+    $installPath = Join-Path $ahk2exeFolder $exeName
+    if ([System.IO.File]::Exists($installPath)) {
+        Show-Message "Install-UPX" "UPX is already installed, skipping re-installation..." $StyleInfo $StyleQuiet
+        return
+    }
+
+    Invoke-UnzipAllInPlace -TaskName "Install-UPX" -FolderPath $downloadFolder
+
+    $upxPath = (Get-ChildItem -Path $downloadFolder -Recurse -Filter $exeName | Select-Object -First 1)
+    if ([string]::IsNullOrEmpty($upxPath)) { Throw "Missing UPX Executable '$upxPath'." }
+
+    Show-Message "Install-UPX" "Copying UPX executable into Ahk2Exe directory..." $StyleInfo $StyleAction
+    Show-Message "Install-UPX" "Source: $upxPath" $StyleInfo $StyleCommand
+    Show-Message "Install-UPX" "Destination: $installPath" $StyleInfo $StyleCommand
+    Move-Item -Path $upxPath -Destination $installPath -Force
+
+    Show-Message "Install-UPX" "Verifying installation..." $StyleInfo $StyleAction
+    if (![System.IO.File]::Exists($installPath)) { throw "Failed to install UPX. File was not present in Ahk2Exe folder after installation step completed." }
+    Show-Message "Install-UPX" "Installation path: $installPath" $StyleInfo $StyleCommand
+    Show-Message "Install-UPX" "Installation completed" $StyleInfo $StyleStatus
+    return $installPath
+}
+
+function Invoke-Ahk2Exe {
+    param (
+        [string]$Path,
+        [string]$In,
+        [string]$Out,
+        [string]$Icon,
+        [string]$Base,
+        [string]$Compression = 'upx',
+        [string]$ResourceId
+    )
+    Show-Message "Build $Out" "Converting $In to $Out..." $StyleInfo $StyleAction
+
+    $ahk2exe_args = "/silent verbose /in `"$In`""
+    $ahk2exe_args += " /base `"$Base`""
+
+    Switch ($compression) {
+        'none' { $ahk2exe_args += " /compress 0" }
+        'upx'  { $ahk2exe_args += " /compress 2" } 
+        Default { Throw "Unsupported Compression Type: '$compression'. Valid Options: none, upx"}
+    }
+    
+    if (![string]::IsNullOrEmpty($Out)) { 
+        [void](New-Item -Path $Out -ItemType File -Force)
+        $ahk2exe_args += " /out `"$Out`"" 
+    }
+    $ahk2exe_args += if (![string]::IsNullOrEmpty($Icon)) { " /icon `"$Icon`"" }
+    $ahk2exe_args += if (![string]::IsNullOrEmpty($ResourceId)) { " /resourceid `"$ResourceId`"" }
+
+    $command = "Start-Process -NoNewWindow -PassThru -FilePath `"$Path`" -ArgumentList '$ahk2exe_args'"
+
+    Show-Message "Build $Out" "`"$command`"" $StyleInfo $StyleCommand
+    $process = Invoke-Expression "$command"
+    $process | Wait-Process -Timeout 300
+    if ($process.ExitCode -ne 0) {
+        Throw "Exception occurred during build."
+    } else {
+        Show-Message "Build-$Out" "Build completed" $StyleInfo $StyleStatus
+    }
+}
+
+function Invoke-Action {
+	Show-Message "Action-Started" "" $StyleStatus
+    
+    $ahkPath = Install-AutoHotkey
+    $ahk2exePath = Install-Ahk2Exe
+
+	if ("$env:Compression" -eq "upx") {
+	    [void](Install-UPX -Ahk2ExePath $ahk2exePath)
+	}
+	
+	Invoke-Ahk2Exe -Path "$ahk2exePath" -Base "$ahkPath" -In "$env:In" -Out "$env:Out" -Icon "$env:Icon" -Compression "$env:Compression" -ResourceId "$env:ResourceId"
+	Show-Message "Action-Finished" "" $StyleStatus
+}
+	
+Invoke-Action
